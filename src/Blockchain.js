ca7fd497f44bb6c8767203c11481481d74b06818
aidatorajiro <kawarusosu@zoho.com>
2018-07-06 19:25:45 +0900

    modify comment

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -136,10 +136,10 @@ export default class BlockChain {
 
   /**
    * Verify a block. It checks:
-   * (1) calculated minimum target < hash of the header < header.target
-   * (2) header.treeHash == merkle hash of the transactions
-   * (3) header.prevHash == last block hash
-   * (4) last timestamp < header.timestamp < timestamp tolerance + current unix time
+   * 1. calculated minimum target < hash of the header < header.target
+   * 2. header.treeHash == merkle hash of the transactions
+   * 3. header.prevHash == last block hash
+   * 4. last timestamp < header.timestamp < timestamp tolerance + current unix time
    * @param {Object} blockheader header of the block
    * @param {Object[]} transactions transaction list of the block
    * @returns Bool
