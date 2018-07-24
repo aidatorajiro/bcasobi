091ca630b192e527700f9cfcf882bfe0606401c9
Aida Torajiro <kawarusosu@zoho.com>
2018-07-24 19:07:55 +0900

    add network

---
+++ src/network/network.js
@@ -0,0 +1,18 @@
+/* 
+ * Usage (A simple paxos implementation):
+ * ```
+ * let = new Peer('validator')
+ *
+ * ```
+ */
+
+export class Peer () {
+}
+
+/*
+ * @member roles List of roles of the network.
+ * @member 
+ */
+export class Network () {
+  
+}
