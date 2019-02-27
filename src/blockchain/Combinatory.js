7d878a4e85d88dc1b7db81d09a35481743dffec5
aidatorajiro <kawarusosu@zoho.com>
2019-02-27 09:24:49 +0900

    modify Combinatory.js

--- src/blockchain/Combinatory.js
+++ src/blockchain/Combinatory.js
@@ -4,20 +4,21 @@ import JSSHA from 'jssha'
 export default class Combinatory extends BlockChain {
   constructor () {
     super()
-    this.state = {}
+    this.hashToPath = {}
+    
     addNode([])
   }
   updateState () {
     let transactions = this.transactions[this.transactions.length - 1]
     for (let i = 0; i < transactions.length; i++) {
       let tx = transactions[i]
-      addNode(this.state(tx.hash).concat(tx.func))
+      addNode(this.hashToPath[tx.hash].concat(tx.func))
     }
   }
   addNode (arr) {
     let shaObj = new JSSHA('SHA-256', 'BYTES')
     shaObj.update(arr)
     let hash = shaObj.getHash('HEX')
-    this.state[hash] = arr
+    this.hashToPath[hash] = arr
   }
 }
